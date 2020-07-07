8fc4af7ac7ca3d885e25b67b1065f8c0eb31c0e2
Anusha Panta <60990466+aanusa05@users.noreply.github.com>
2020-07-06 20:49:48 -0500

    Add files via upload

---
+++ src/main/java/com/gadget/demo/GadgetRepairApplication.java
@@ -0,0 +1,13 @@
+package com.gadget.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadgetRepairApplication.class, args);
+	}
+
+}
